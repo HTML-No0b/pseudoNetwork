2c20874a21a180df7c3d75571b538a8e1535e683
Html-No0b <rmartin9797@gmail.com>
2023-02-13 02:45:00 -0600

    added to model folders

--- models/user.js
+++ models/user.js
@@ -17,9 +17,20 @@ const userSchema = new Schema({
         }
     },
     thoughts:{
+        type:mongoose.Types.ObjectId,
+        ref:'thought'
 
     },
     friends:{
+        type:mongoose.Types.ObjectId,
+        ref:'user'
         
     }
-})
+});
+userSchema.virtual('friendCount').get(()=>{
+    return this.friends.length;
+});
+
+const user = model ('user',userSchema);
+
+module.exports = user;
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
+
 
 const app = express ();
-const PORT = porcess.env.PORT || 3001
+const PORT = porcess.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+const mongoose = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/networkDB'
+mongoose.connect(
+    mongouri.at,
+    {
+        useNewUrlParser: True,
+        useUnifiedTopology:true
+    }
+);
