34c17f79e895ecd5eddcf7564b43b77f21124684
Html-No0b <rmartin9797@gmail.com>
2023-02-13 03:25:27 -0600

    added json packages

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require('mongoose');
+require("dotenv").config();
 
-
-const app = express ();
-const PORT = porcess.env.PORT || 3001;
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-const mongoose = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/networkDB'
 mongoose.connect(
-    mongouri.at,
-    {
-        useNewUrlParser: True,
-        useUnifiedTopology:true
-    }
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/network_db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+
